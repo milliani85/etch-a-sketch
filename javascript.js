04c7f737f972968ce6865fc8ece7f5baabe985eb
Robert Miller <robertmiller85@live.co.uk>
2022-07-28 20:28:12 +0100

    Add a 16x16 grid. Divs created with javascript loop. Contained in a HTML div and arranged with flex.

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let container = document.querySelector('.container');
+
+for(i = 0; i < 256; i++) {
+let grid = document.createElement('div');
+  grid.classList.add("grid");
+    container.appendChild(grid);
+
+}
