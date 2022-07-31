2e7d4b45d26fe30c064e79365c3c4d900499e3f4
Robert Miller <robertmiller85@live.co.uk>
2022-07-31 20:46:43 +0100

    Add feature that changes colour of grids to black when mouse is hovered over. Add a select gris size button with prompt, not yet able to change grid size

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
-let container = document.querySelector('.container');
+const container = document.querySelector('.grid-container');
+let size;
+
+const gridSize = document.getElementById('grid-size');
+  gridSize.addEventListener('click', (e) => {
+      size = prompt('Pick your grid size');
+  })
+
+
 
 for(i = 0; i < 256; i++) {
-let grid = document.createElement('div');
-  grid.classList.add("grid");
-    container.appendChild(grid);
+  const grid = document.createElement('div');
+    grid.classList.add('grid');
+      container.appendChild(grid);
+        //grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+         // grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+            grid.addEventListener('mouseover', (e) => {
+              e.target.style.backgroundColor = 'black';
 
+        })
 }
+
